4477af723de99803d5913f41c1089c23a82a5417
sisisin <azsisisin@gmail.com>
2023-03-01 23:29:43 +0900

    feat: page 3
    https://build-your-own.org/redis/03_hello_cs

---
+++ Makefile
@@ -0,0 +1,3 @@
+build:
+	 g++ -Wall -Wextra -O2 -g ./src/server.cpp -o server
+	 g++ -Wall -Wextra -O2 -g ./src/client.cpp -o client
---
+++ src/server.cpp
@@ -0,0 +1,74 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+
+static void msg(const char *msg)
+{
+  fprintf(stderr, "%s\n", msg);
+}
+static void die(const char *msg)
+{
+  int err = errno;
+  fprintf(stderr, "[%d] %s\n", err, msg);
+  abort();
+}
+
+static void do_something(int conn_fd)
+{
+  char rbuf[64] = {};
+  ssize_t n = read(conn_fd, rbuf, sizeof(rbuf) - 1);
+
+  if (n < 0)
+  {
+    msg("read() error");
+    return;
+  }
+  printf("client says: %s\n", rbuf);
+
+  char wbuf[] = "world";
+  write(conn_fd, wbuf, strlen(wbuf));
+}
+
+int main()
+{
+  int fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (fd < 0)
+  {
+    die("socket()");
+  }
+
+  int val = 1;
+  setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
+  struct sockaddr_in addr = {};
+  addr.sin_family = AF_INET;
+  addr.sin_port = ntohs(1234);
+  addr.sin_addr.s_addr = ntohl(0);
+  int rv = bind(fd, (const sockaddr *)&addr, sizeof(addr));
+  if (rv)
+  {
+    die("bind()");
+  }
+  rv = listen(fd, SOMAXCONN);
+
+  while (true)
+  {
+    struct sockaddr_in client_addr = {};
+    socklen_t socklen = sizeof(client_addr);
+    int conn_fd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
+    if (conn_fd < 0)
+    {
+      continue;
+    }
+
+    do_something(conn_fd);
+    close(conn_fd);
+  }
+  return 0;
+}
